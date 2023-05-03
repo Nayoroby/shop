766f74370d513394aa337eb060a8ac60296c7032
max <maxmakarow86@gmail.com>
2023-05-03 21:24:21 +0300

    new git

--- src/card/card.jsx
+++ src/card/card.jsx
@@ -1,5 +1,15 @@
 import React, { useState } from 'react';
 import "./card.scss";
+import apple from "../../public/apple.jpg";
+import orange from "../../public/orange.jpg";
+import banana from "../../public/banana.jpg";
+import raspberries from "../../public/raspberries.jpg";
+import cherry from "../../public/cherry.jpg";
+import melon from "../../public/melon.jpg";
+import pear from "../../public/pear.jpg";
+import strawberry from "../../public/strawberry.jpg";
+import watermelon from "../../public/watermelon.jpg";
+import kiwi from "../../public/kiwi.jpg";
 
 
 const Card = ({addCartItem, title, img, price, id}) => {
